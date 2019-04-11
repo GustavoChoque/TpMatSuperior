f774a362d38aa17ffa8c3864d8bb46ce5af187dc
GustavoChoque <e_gustavo14@hotmail.com>
2019-04-10 22:31:18 -0300

    primer Commit

---
+++ TPNCOM.m
@@ -0,0 +1,15 @@
+disp("Bienvenido a NCOM\n");
+disp("Menu de Opciones");
+disp("1.Suma de Nº Complejos");
+disp("0.Salir");
+opc=input("seleccione una opcion: ")
+
+if(opc==1)
+  comp1=input("ingrese el primer Nº complejo");
+  comp2=input("ingrese el segundo Nº complejo");
+  
+endif
+
+function sumaComplejos(num1,num2)
+  
+endfunction
